cd9fcfe24efd0914fe92723f52ed00d8f217b1f1
Bradley Gruetzman <115727834+BradleyGruetzman@users.noreply.github.com>
2025-11-19 16:35:46 -0600

    Delete aws-weather-etl-pipeline directory

--- aws-weather-etl-pipeline/sql/create_tables.sql
+++
@@ -1,9 +0,0 @@
--- Weather Data Table
-CREATE TABLE IF NOT EXISTS weather (
-    id SERIAL PRIMARY KEY,
-    city TEXT NOT NULL,
-    temperature DOUBLE PRECISION,
-    humidity BIGINT,
-    description TEXT,
-    timestamp TEXT
-);
